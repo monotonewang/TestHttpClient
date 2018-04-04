725b70bf79cf775805435c88392574d04e1e9836
monotonewang <zgjxfuqingwang@gmail.com>
2018-04-05 06:04:00 +0800

    x

--- src/test/java/com/example/ubuntu/testhttpclient/weakreference/TestWeakReference.java
+++ src/test/java/com/example/ubuntu/testhttpclient/weakreference/TestWeakReference.java
@@ -8,19 +8,52 @@ import java.lang.ref.WeakReference;
 public class TestWeakReference {
 
     @Test
-    public void testWeak(){
-        WeakReference<People> reference=new WeakReference<People>(new People("s",1));
-        System.out.println(reference.get());
+    public void testStrong(){
+        People reference=new People("s",1);
+        System.out.println(reference);
+        long initm=Runtime.getRuntime().freeMemory();
+        System.out.println("initm="+initm);
+        reference=null;
+//        reference.clear();
         System.gc();
-        System.out.println(reference.get());
+        if(reference!=null) {
+            System.out.println(reference);
+        }
+        long end = Runtime.getRuntime().freeMemory();
+        System.out.println("end="+end);
     }
 
     @Test
     public void testSoft(){
         SoftReference<People> reference=new SoftReference<People>(new People("s",1));
+        long initm=Runtime.getRuntime().freeMemory();
+        System.out.println("initm="+initm);
         System.out.println(reference.get());
-        reference=null;
+//        reference.clear();
         System.gc();
         System.out.println(reference.get());
+        long end=Runtime.getRuntime().freeMemory();
+        System.out.println("end"+end);
     }
+
+    @Test
+    public void testWeak(){
+        WeakReference<People> reference=new WeakReference<People>(new People("s",1));
+        System.out.println(reference.get());
+        long initm=Runtime.getRuntime().freeMemory()/1024/1024;
+        long totalMemory=Runtime.getRuntime().totalMemory()/1024/1024;
+        long maxMemory=Runtime.getRuntime().maxMemory()/1024/1024;
+        System.out.println("initm="+initm+"total"+totalMemory+"totalMemory"+"maxMemory="+maxMemory);
+//        reference.clear();
+        System.gc();
+        System.out.println(reference.get());
+        long end=Runtime.getRuntime().freeMemory()/1024/1024;
+        long totalMemoryx=Runtime.getRuntime().totalMemory()/1024/1024;
+        long maxMemoryx=Runtime.getRuntime().maxMemory()/1024/1024;
+        System.out.println("end="+end+"total"+totalMemoryx+"totalMemoryx"+"maxMemoryx="+maxMemoryx);
+    }
+
+
+
+
 }
